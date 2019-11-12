40f346a4f8f62fd6f501a5b93ebc475c4553a81c
Kasim Rehman <57490380+kasimrehman@users.noreply.github.com>
2019-11-12 11:03:14 +0100

    removing unecessary todos (#22)

--- apps/onboarding-skill/src/services/onboarding/messaginginterface/Subscription.ts
+++ apps/onboarding-skill/src/services/onboarding/messaginginterface/Subscription.ts
@@ -4,15 +4,4 @@ class Subscription {
   constructor(public topic: string, public messageReceiver: IMessageReceiver) {}
 }
 
-/*
-class AmqpSubscription extends Subscription {
-  constructor(
-    topic: string,
-    messageReceiver: IMessageReceiver,
-    public brokerExchange: string
-  ) {
-    super(topic, messageReceiver);
-  }
-}*/
-
 export { Subscription };
