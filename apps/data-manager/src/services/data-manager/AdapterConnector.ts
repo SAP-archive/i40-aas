ead15748e340f0632a71dab58970d5bf0f68b245
Manu3756 <sauermanuel95@web.de>
2019-12-16 16:35:54 +0100

    add GET /submodes to router

--- apps/data-manager/src/services/data-manager/AdapterConnector.ts
+++ apps/data-manager/src/services/data-manager/AdapterConnector.ts
@@ -3,33 +3,30 @@ import * as logger from "winston";
 import { WebClient } from "./WebClient/WebClient";
 import { IStorageAdapter } from "./interfaces/IStorageAdapter";
 
+class AdapterConnector {
+  private webClient = new WebClient();
 
+  constructor(wC: WebClient) {
+    this.webClient = wC;
+  }
 
-class AdapterConnector{
-private webClient = new WebClient();
+  /**
+   * Make a post request to the storage adapter responsible for handling the Submodel type
+   * @param sm The submodel element
+   * @param adapter The storage adapter object
+   */
+  async postSubmoduleToAdapter(sm: Submodel, adapter: IStorageAdapter) {
+    //get the url of the storage adapter from the object
+    let adapter_url: string = adapter.url;
 
+    return await this.webClient.postRequest(adapter_url, sm);
+  }
+  async getSubmoduleFromAdapter(adapter: IStorageAdapter) {
+    //get the url of the storage adapter from the object
+    let adapter_url: string = adapter.url;
 
-constructor(wC:WebClient){
-this.webClient = wC;
-
+    return await this.webClient.getRequest(adapter_url, "");
+  }
 }
 
-
-/**
- * Make a post request to the storage adapter responsible for handling the Submodel type
- * @param sm The submodel element
- * @param adapter The storage adapter object
- */
-async postSubmoduleToAdapter(
-  sm: Submodel,
-  adapter: IStorageAdapter
-) {
-  //get the url of the storage adapter from the object
-  let adapter_url: string = adapter.url;
-
-  return await this.webClient.postRequest(adapter_url, sm);
-}
-
-}
-
-export {AdapterConnector}
+export { AdapterConnector };
