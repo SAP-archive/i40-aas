73654cc1a3ef6eec0335b75b2b3c169e7d8937d0
akiskips <orthodoxos.kipouridis@sap.com>
2020-02-06 15:48:32 +0100

    fix format

--- apps/endpoint-registry/sql/sampleRequestByRole.sql
+++ apps/endpoint-registry/sql/sampleRequestByRole.sql
@@ -2,11 +2,11 @@
 SELECT "aasId", "URL", "protocol_name", "protocol_version", "roleId"
 FROM (SELECT *
     FROM public.aas_role
-        INNER JOIN public.asset_administration_shells 
-USING ("aasId") 
+        INNER JOIN public.asset_administration_shells
+USING ("aasId")
     WHERE "roleId" = (SELECT "roleId"
     FROM public.roles
-    where 
+    where
 "protocolId" = 'i40:registry-semanticProtocol/onboarding' and "roleId" = 'Approver'
 limit
 1)) as res
