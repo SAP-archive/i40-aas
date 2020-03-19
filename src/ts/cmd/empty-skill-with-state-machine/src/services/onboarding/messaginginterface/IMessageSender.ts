ad349b5650b137abe9242b5ef91bb579abbc1952
heldic <helge@dickel.io>
2020-03-19 19:46:43 +0100

    remove empty-skill

--- src/ts/cmd/empty-skill-with-state-machine/src/services/onboarding/messaginginterface/IMessageSender.ts
+++
@@ -1,8 +0,0 @@
-import { IFrame } from "i40-aas-objects";
-
-interface IMessageSender {
-  replyTo(frame: IFrame, type: string, submodels?: object[]): void;
-  sendTo(frame: IFrame, submodels?: object[]): void;
-  start(notifyReady?: () => void): void;
-}
-export { IMessageSender };
--- src/ts/cmd/empty-skill-with-state-machine/src/services/onboarding/messaginginterface/Subscription.ts
+++
@@ -1,7 +0,0 @@
-import { IMessageReceiver } from "./IMessageReceiver";
-
-class Subscription {
-  constructor(public topic: string, public messageReceiver: IMessageReceiver) {}
-}
-
-export { Subscription };
