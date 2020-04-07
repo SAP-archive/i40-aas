0c627d6ed33b478bf04e95f3772fe8d4e90a1238
akiskips <orthodoxos.kipouridis@sap.com>
2020-04-07 22:12:52 +0200

    update http-egress test env

--- src/ts/cmd/https-endpoint-egress/src/server.ts
+++ src/ts/cmd/https-endpoint-egress/src/server.ts
@@ -2,37 +2,16 @@ import { AmqpClient } from 'AMQP-Client/lib/AmqpClient';
 import { BrokerMessageInterpreter } from "./messaging/BrokerMessageInterpreter";
 import { logger } from "./utils/log";
 
-// if (process.env.NODE_ENV !== 'production') {
-const dotenv = require('dotenv');
-dotenv.config();
 
 // Message broker Config
-let CORE_BROKER_HOST: string | undefined = process.env.CORE_BROKER_HOST;
-let CORE_BROKER_PORT: string | undefined = process.env.CORE_BROKER_PORT;
-let CORE_EGRESS_EXCHANGE: string | undefined = process.env.CORE_EGRESS_EXCHANGE;
-let CORE_EGRESS_USER: string | undefined = process.env.CORE_EGRESS_USER;
-let CORE_EGRESS_PASSWORD: string | undefined = process.env.CORE_EGRESS_PASSWORD;
-let CORE_EGRESS_HTTP_QUEUE: string | undefined =
-  process.env.CORE_EGRESS_HTTP_QUEUE;
-let CORE_EGRESS_HTTP_BROKER_BINDINGKEY =
-  CORE_EGRESS_EXCHANGE + '.' + CORE_EGRESS_HTTP_QUEUE;
+let CORE_BROKER_HOST= checkEnvVar('CORE_BROKER_HOST');
+let CORE_BROKER_PORT= checkEnvVar('CORE_BROKER_PORT');
+let CORE_EGRESS_EXCHANGE = checkEnvVar('CORE_EGRESS_EXCHANGE');
+let CORE_EGRESS_USER = checkEnvVar('CORE_EGRESS_USER');
+let CORE_EGRESS_PASSWORD = checkEnvVar('CORE_EGRESS_PASSWORD');
+let CORE_EGRESS_HTTP_QUEUE= checkEnvVar('CORE_EGRESS_HTTP_QUEUE');
+let CORE_EGRESS_HTTP_BROKER_BINDINGKEY = CORE_EGRESS_EXCHANGE + "." + CORE_EGRESS_HTTP_QUEUE
 
-logger.debug('Env Variable CORE_BROKER_HOST: ' + CORE_BROKER_HOST);
-logger.debug('Env Variable CORE_EGRESS_EXCHANGE: ' + CORE_EGRESS_EXCHANGE);
-logger.debug(
-  'Env Variable CORE_EGRESS_HTTP_BROKER_BINDINGKEY: ' +
-    CORE_EGRESS_HTTP_BROKER_BINDINGKEY
-);
-
-if (
-  CORE_BROKER_HOST &&
-  CORE_BROKER_PORT &&
-  CORE_EGRESS_EXCHANGE &&
-  CORE_EGRESS_HTTP_BROKER_BINDINGKEY &&
-  CORE_EGRESS_HTTP_QUEUE &&
-  CORE_EGRESS_USER &&
-  CORE_EGRESS_PASSWORD
-) {
   var brokerClient = new AmqpClient(
     CORE_BROKER_HOST,
     CORE_BROKER_PORT,
@@ -50,6 +29,18 @@ if (
 
   //start listening for messages at the broker
   messageInterpreter.start(CORE_EGRESS_HTTP_BROKER_BINDINGKEY as string);
-} else {
-  logger.error('One or more env variable not set, service not started');
+
+
+
+
+function checkEnvVar(variableName: string): string {
+  let retVal: string | undefined = process.env[variableName];
+  if (retVal) {
+    return retVal;
+  } else {
+    throw new Error(
+      'A variable that is required by the service has not been defined in the environment:' +
+	variableName
+    );
+  }
 }
