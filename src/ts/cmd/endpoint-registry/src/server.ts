9d49403915dbbdd37e061e371b0849acfdeda41a
kasimrehman <kasim.rehman@sap.com>
2020-04-15 14:32:53 +0200

    first unit test

--- src/ts/cmd/endpoint-registry/src/server.ts
+++ src/ts/cmd/endpoint-registry/src/server.ts
@@ -5,7 +5,7 @@ import healthRoute from './services/health/routes';
 
 import routes from './services';
 import middleware from './middleware';
-import "reflect-metadata";
+import 'reflect-metadata';
 import { RegistryFactory } from './services/registry/daos/postgres/RegistryFactory';
 
 const router = express();
@@ -13,10 +13,11 @@ applyRoutes(healthRoute, router);
 applyMiddleware(middleware, router);
 applyRoutes(routes, router);
 
-
 const PORT = 4400;
 const server = http.createServer(router);
 
+server.listen(PORT, () =>
+  console.log(`A Server is running http://localhost:${PORT}...`)
+);
 
-
-server.listen(PORT, () => console.log(`A Server is running http://localhost:${PORT}...`));
+export { router as app };
