fe2c9d8ffb8436a732593ba4b506c9a3f23d214c
Eike-Tobias Ostermann <eike-tobias.ostermann@sap.com>
2020-04-17 09:50:03 +0200

    CI Redesign with Tests
    Major redesign on the Travis CI pipeline.
    Includes updates on some services to bring tests up to date.
    Also includes updates on Kubernetes manifests.

--- src/ts/cmd/data-manager/src/server.ts
+++ src/ts/cmd/data-manager/src/server.ts
@@ -11,24 +11,22 @@ import { RoutingController } from "./services/data-manager/RoutingController";
 import { AdapterConnector } from "./services/data-manager/AdapterConnector";
 import { AdapterRegistryConnector } from "./services/data-manager/RegistryConnector";
 
-const dotenv = require("dotenv");
-dotenv.config();
-
-let CORE_REGISTRIES_ADAPTERS_PROTOCOL = process.env.CORE_REGISTRIES_ADAPTERS_PROTOCOL;
-let CORE_REGISTRIES_ADAPTERS_HOST = process.env.CORE_REGISTRIES_ADAPTERS_HOST;
-let CORE_REGISTRIES_ADAPTERS_PORT = process.env.CORE_REGISTRIES_ADAPTERS_PORT;
-let CORE_REGISTRIES_ADAPTERS_USER = process.env.CORE_REGISTRIES_ADAPTERS_USER;
-let CORE_REGISTRIES_ADAPTERS_PASSWORD = process.env.CORE_REGISTRIES_ADAPTERS_PASSWORD;
-let CORE_REGISTRIES_ADAPTERS_URL_SUFFIX = process.env.CORE_REGISTRIES_ADAPTERS_URL_SUFFIX;
+let CORE_DATA_MANAGER_PORT = checkEnvVar('CORE_DATA_MANAGER_PORT');
+let CORE_REGISTRIES_ADAPTERS_PROTOCOL = checkEnvVar('CORE_REGISTRIES_ADAPTERS_PROTOCOL');
+let CORE_REGISTRIES_ADAPTERS_HOST = checkEnvVar('CORE_REGISTRIES_ADAPTERS_HOST');
+let CORE_REGISTRIES_ADAPTERS_PORT = checkEnvVar('CORE_REGISTRIES_ADAPTERS_PORT');
+let CORE_REGISTRIES_ADAPTERS_USER = checkEnvVar('CORE_REGISTRIES_ADAPTERS_USER');
+let CORE_REGISTRIES_ADAPTERS_PASSWORD = checkEnvVar('CORE_REGISTRIES_ADAPTERS_PASSWORD');
+let CORE_REGISTRIES_ADAPTERS_URL_SUFFIX = checkEnvVar('CORE_REGISTRIES_ADAPTERS_URL_SUFFIX');
 var webClient = new WebClient();
 
 process.on("uncaughtException", e => {
-  logger.error(e);
+  logger.error("Uncaught Exception " + e);
   process.exit(1);
 });
 
 process.on("unhandledRejection", e => {
-  logger.error(e as string);
+  logger.error("Unhandled Rejection" +e as string);
   process.exit(1);
 });
 
@@ -45,7 +43,6 @@ applyRoutes(routes, router);
 //error handling
 applyMiddleware(errorHandlers, router);
 
-const PORT = 4000 ;
 const server = http.createServer(router);
 
 var webClient = new WebClient();
@@ -56,14 +53,6 @@ var buildUrl = (protocol:string,
 
       return protocol+"://"+host+":"+port+suffix;
     }
-if (
-  CORE_REGISTRIES_ADAPTERS_PROTOCOL &&
-  CORE_REGISTRIES_ADAPTERS_HOST &&
-  CORE_REGISTRIES_ADAPTERS_PORT &&
-  CORE_REGISTRIES_ADAPTERS_USER &&
-  CORE_REGISTRIES_ADAPTERS_PASSWORD &&
-  CORE_REGISTRIES_ADAPTERS_URL_SUFFIX
-) {
 
 let storageAdapterRegistryURL = new URL(buildUrl(CORE_REGISTRIES_ADAPTERS_PROTOCOL,CORE_REGISTRIES_ADAPTERS_HOST,CORE_REGISTRIES_ADAPTERS_PORT,CORE_REGISTRIES_ADAPTERS_URL_SUFFIX));
 
@@ -75,14 +64,25 @@ let storageAdapterRegistryURL = new URL(buildUrl(CORE_REGISTRIES_ADAPTERS_PROTOC
     CORE_REGISTRIES_ADAPTERS_PASSWORD
   );
   RoutingController.initController(registryConnector, adapterConnector);
-}
-else{
-  logger.error("One or more env. variable was not set. Exiting ");
-  throw new Error();
+
+server.listen(CORE_DATA_MANAGER_PORT, () =>
+  logger.info(`A Server is running http://localhost:${CORE_DATA_MANAGER_PORT} ...`)
+);
+
+
+
+function checkEnvVar(variableName: string): string {
+  let retVal: string | undefined = process.env[variableName];
+  if (retVal) {
+    return retVal;
+  } else {
+    throw new Error(
+      'A variable that is required by the service has not been defined in the environment:' +
+        variableName
+    );
+  }
 }
 
-server.listen(PORT, () =>
-  logger.info(`A Server is running http://localhost:${PORT} ...`)
-);
+
 
 export { router as app };
