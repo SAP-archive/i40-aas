4a8d4ebb91d0ba0b98f6120ac0396a9d890d47db
kasimrehman <kasim.rehman@sap.com>
2020-04-23 09:18:31 +0200

    wiP

--- src/ts/cmd/https-endpoint-ingress/src/middleware/checks.ts
+++ src/ts/cmd/https-endpoint-ingress/src/middleware/checks.ts
@@ -1,29 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import { HTTP404Error, HTTP400Error, HTTP422Error } from '../utils/httpErrors';
+import { IInteractionMessage, InteractionMessage } from 'i40-aas-objects';
 
-import { Request, Response, NextFunction } from "express";
-import { HTTP404Error, HTTP400Error, HTTP422Error } from "../utils/httpErrors";
-import { IInteractionMessage, InteractionMessage } from "i40-aas-objects";
-import { logger } from "../utils/log";
-
+const logger = require('aas-logger/lib/log');
 
 export const validateInteractionFrame = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-    //logger.debug("RECEIVED Message: " + req.body)
+  //logger.debug("RECEIVED Message: " + req.body)
 
-    try {
-      let interactionReq:IInteractionMessage = new InteractionMessage(req.body);
+  try {
+    let interactionReq: IInteractionMessage = new InteractionMessage(req.body);
 
-      //the required fields of the frame that should not be empty
-      let semanticProtocol:string = interactionReq.frame.semanticProtocol;
-      let receiverRole:string = interactionReq.frame.receiver.role.name;
-      let senderRole:string = interactionReq.frame.receiver.role.name;
-      let type:string = interactionReq.frame.type;
-      let conversationId:string = interactionReq.frame.conversationId;
+    //the required fields of the frame that should not be empty
+    let semanticProtocol: string = interactionReq.frame.semanticProtocol;
+    let receiverRole: string = interactionReq.frame.receiver.role.name;
+    let senderRole: string = interactionReq.frame.receiver.role.name;
+    let type: string = interactionReq.frame.type;
+    let conversationId: string = interactionReq.frame.conversationId;
 
-      next();
-    } catch (error) {
-      throw new HTTP422Error("Missing required fields in Interaction Frame: "+ error);
-    }
+    next();
+  } catch (error) {
+    throw new HTTP422Error(
+      'Missing required fields in Interaction Frame: ' + error
+    );
+  }
 };
