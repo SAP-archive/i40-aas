ffe5eeaa183c5503820813000d84f47e93a60330
etoster <eike-tobias.ostermann@sap.com>
2020-07-30 14:51:13 +0200

    ready java server and client

--- src/proto/interaction/examples/java/client/src/main/java/sap/i40aas/interaction/client/InteractionClient.java
+++ src/proto/interaction/examples/java/client/src/main/java/sap/i40aas/interaction/client/InteractionClient.java
@@ -1,18 +1,88 @@
 package sap.i40aas.interaction.client;
 
-import io.grpc.Channel;
+import sap.i40aas.interaction.proto.*;
+
+import com.google.protobuf.util.JsonFormat;
+
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import sap.i40aas.interaction.proto.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class InteractionClient {
-  private static final Logger logger = Logger.getLogger(InteractionClient.class.getName());
-  public static void main( String[] args ) {
-    System.out.println( "Hello World!" );
-  }
+
+    public static void main(final String args[]){
+
+      JSONParser jsonParser = new JSONParser();
+
+      try (FileReader reader = new FileReader("/home/chief/i40-aas/opensource/i40-aas/src/proto/interaction/examples/sample_interaction.json"))
+      {
+          //Read JSON file
+          Object obj = jsonParser.parse(reader);
+
+          // Construct an InteractionMessage Builder, as described here:
+          // https://developers.google.com/protocol-buffers/docs/reference/java-generated#builders
+          InteractionMessage.Builder interactionBuilder = InteractionMessage.newBuilder();
+
+          // Fill InteractionMessage builder with JSON content
+          JsonFormat.parser().merge(obj.toString(), interactionBuilder);
+
+          // Construct the actual InteractionMessage object
+          InteractionMessage interactionMessage = interactionBuilder.build();
+          // TODO: check that this is really the correct message format and correctly filled.
+
+          System.out.println("Hello from Client!");
+
+          // Create a communication channel to the server, known as a Channel. Channels are thread-safe
+          // and reusable. It is common to create channels at the beginning of your application and reuse
+          // them until the application shuts down.
+          String target = "localhost:50051";
+          ManagedChannel channel = ManagedChannelBuilder.forTarget(target)
+          // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+          // needing certificates.
+          .usePlaintext()
+          .build();
+          try {
+            // Tobi: I took this way of building the client from this example:
+            // https://github.com/grpc/grpc-java/blob/v1.30.0/examples/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
+            // Also, for more general info see: https://grpc.io/docs/languages/java/basics/#client
+
+            // Tobi: However, I didn't managed to correctly compile the protobuf classes for java
+            // _ and I assume that's way newBlockingStub and channel aren't matching as they are in the example:
+            InteractionIngressGrpc.InteractionIngressBlockingStub blockingStub = InteractionIngressGrpc.newBlockingStub(channel);
+
+            try {
+              InteractionStatus response = blockingStub.sendInteractionMessage(interactionMessage);
+            } catch (StatusRuntimeException e) {
+              System.out.println( "RPC failed: {0}" + e.getStatus().toString());
+              return;
+            }
+          } finally {
+            // ManagedChannels use resources like threads and TCP connections. To prevent leaking these
+            // resources the channel should be shut down when it will no longer be used. If it may be used
+            // again leave it running.
+            try {
+              channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+            } catch (InterruptedException ie) {
+              System.out.println("Channel termination failed" + ie.getMessage());
+            }
+          }
+
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      } catch (IOException e) {
+          e.printStackTrace();
+      } catch (ParseException e) {
+          e.printStackTrace();
+      }
+
+
+    }
+
+
 }
--- src/proto/interaction/examples/java/server/src/main/java/sap/i40aas/interaction/server/InteractionServer.java
+++ src/proto/interaction/examples/java/server/src/main/java/sap/i40aas/interaction/server/InteractionServer.java
@@ -11,7 +11,62 @@ import sap.i40aas.interaction.proto.*;
 
 public class InteractionServer {
   private static final Logger logger = Logger.getLogger(InteractionServer.class.getName());
-  public static void main( String[] args ) {
-    System.out.println( "Hello World!" );
+
+  private Server server;
+
+  private void start() throws IOException {
+    /* The port on which the server should run */
+    int port = 50051;
+    server = ServerBuilder.forPort(port)
+        .addService(new InteractionIngressImpl())
+        .build()
+        .start();
+    logger.info("Server started, listening on " + port);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+        System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        try {
+          InteractionServer.this.stop();
+        } catch (InterruptedException e) {
+          e.printStackTrace(System.err);
+        }
+        System.err.println("*** server shut down");
+      }
+    });
+  }
+
+  private void stop() throws InterruptedException {
+    if (server != null) {
+      server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+    }
+  }
+
+  /**
+   * Await termination on the main thread since the grpc library uses daemon threads.
+   */
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    final InteractionServer server = new InteractionServer();
+    server.start();
+    server.blockUntilShutdown();
+  }
+
+  static class InteractionIngressImpl extends InteractionIngressGrpc.InteractionIngressImplBase {
+
+    @Override
+    public void sendInteractionMessage(InteractionMessage req, StreamObserver<InteractionStatus> responseObserver) {
+      System.out.println(req.toString());
+      InteractionStatus reply = InteractionStatus.newBuilder().setCodeValue(1).build();
+      responseObserver.onNext(reply);
+      responseObserver.onCompleted();
+    }
   }
 }
