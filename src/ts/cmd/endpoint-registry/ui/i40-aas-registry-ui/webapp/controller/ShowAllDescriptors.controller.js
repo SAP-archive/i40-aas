51f2a6afd3f067875ba119592a8b0f8b8aa20245
Nico Strazzeri <nico@strazzeri.de>
2020-11-09 12:27:05 +0100

    AAS-201 Add autorefresh to Show descriptor UI

--- src/ts/cmd/endpoint-registry/ui/i40-aas-registry-ui/webapp/controller/ShowAllDescriptors.controller.js
+++ src/ts/cmd/endpoint-registry/ui/i40-aas-registry-ui/webapp/controller/ShowAllDescriptors.controller.js
@@ -1,72 +1,108 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/routing/History",
-	"sap/m/MessageToast"
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/core/routing/History",
+  "sap/m/MessageToast"
 ], function (Controller, JSONModel, History, MessageToast) {
-	"use strict";
+  "use strict";
 
-	return Controller.extend("i40-aas-registry-ui.i40-aas-registry-ui.controller.ShowAllDescriptors", {
+  return Controller.extend("i40-aas-registry-ui.i40-aas-registry-ui.controller.ShowAllDescriptors", {
 
-		onInit: function () {
-			// set data model on view
+    onInit: function () {
+      this.getView().addEventDelegate({
+        onAfterShow: this.onAfterShow,
+      }, this);
+    },
 
-			var aAASDescriptors = (function () {
-				var aAASDescriptors = null;
-				$.ajax({
-					'async': false,
-					'global': false,
-					'url': "/AASDescriptors",
-					'dataType': "json",
-					'success': function (data) {
-						aAASDescriptors = data;
-					}
-				});
-				return aAASDescriptors;
-			})();
+    onAfterShow: function () {
+      this.initiateModel();
+      this.startAutorefreshModel(30000);
+    },
 
-			var oData = {
-				"AASDescriptorsCollection": aAASDescriptors
-			};
-			var oModel = new JSONModel(oData);
-			this.getView().setModel(oModel, "DescriptorList");
-		},
+    initiateModel: function () {
+      var aAASDescriptors = (function () {
+        var aAASDescriptors = null;
+        $.ajax({
+          'async': false,
+          'global': false,
+          'url': "/AASDescriptors",
+          'dataType': "json",
+          'success': function (data) {
+            aAASDescriptors = data;
+          }
+        });
+        return aAASDescriptors;
+      })();
 
-		// --------------- Begin SingleDescriptor -------------------------
+      var oData = {
+        "AASDescriptorsCollection": aAASDescriptors
+      };
+      var oModel = new JSONModel(oData);
+      this.getView().setModel(oModel, "DescriptorList");
 
-		onOpenSingleDescriptor: function (oEvent) {
-			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			var oItem = oEvent.getSource();
 
-			//eslint no-console: ["error", { allow: ["warn", "error"] }]
-			//console.warn("oItem = " + oItem);
-			//console.warn("AASId = " + oItem.getBindingContext("DescriptorList").getProperty("identification/id"));
-			var AASId = oItem.getBindingContext("DescriptorList").getProperty("identification/id");
-			var AASIdEncoded = encodeURIComponent(AASId);
-			//var AASIdDecoded = decodeURIComponent(AASIdEncoded);
-			//console.warn("AASId = " + AASId);
-			//console.warn("AASIdEncoded = " + AASIdEncoded);
-			//console.warn("AASIdDecoded = " + AASIdDecoded);
 
-			oRouter.navTo("SingleDescriptor", {
-				AASId: AASIdEncoded
-			});
-		},
+    },
 
-		// --------------- End SingleDescriptor ---------------------------
+    startAutorefreshModel: function (timeInMilliseconds) {
+      var self = this;
+      this.intervalHandle = setInterval(function () {
+        self.initiateModel();
+      }, timeInMilliseconds);
 
-		onNavBack: function () {
-			var oHistory = History.getInstance();
-			var sPreviousHash = oHistory.getPreviousHash();
+    },
 
-			if (sPreviousHash !== undefined) {
-				window.history.go(-1);
-			} else {
-				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-				oRouter.navTo("overview", {}, true);
-			}
-		}
+    stopAutorefreshModel: function () {
+      if (this.intervalHandle)
+        clearInterval(this.intervalHandle);
+    },
 
-	});
+    // --------------- Begin SingleDescriptor -------------------------
+
+    onOpenSingleDescriptor: function (oEvent) {
+      var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+      var oItem = oEvent.getSource();
+
+      //eslint no-console: ["error", { allow: ["warn", "error"] }]
+      //console.warn("oItem = " + oItem);
+      //console.warn("AASId = " + oItem.getBindingContext("DescriptorList").getProperty("identification/id"));
+      var AASId = oItem.getBindingContext("DescriptorList").getProperty("identification/id");
+      var AASIdEncoded = encodeURIComponent(AASId);
+      //var AASIdDecoded = decodeURIComponent(AASIdEncoded);
+      //console.warn("AASId = " + AASId);
+      //console.warn("AASIdEncoded = " + AASIdEncoded);
+      //console.warn("AASIdDecoded = " + AASIdDecoded);
+
+      oRouter.navTo("SingleDescriptor", {
+        AASId: AASIdEncoded
+	  });
+
+	  this.stopAutorefreshModel();
+    },
+
+    // --------------- End SingleDescriptor ---------------------------
+
+    onNavBack: function () {
+      var oHistory = History.getInstance();
+      var sPreviousHash = oHistory.getPreviousHash();
+
+      if (sPreviousHash !== undefined) {
+        window.history.go(-1);
+      } else {
+        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        oRouter.navTo("overview", {}, true);
+      }
+      this.stopAutorefreshModel();
+    },
+
+    onExit: function () {
+      // detach delegates
+      this.getView().removeEventDelegate(this._afterShowDelegate);
+      this._afterShowDelegate = null;
+      // stopAutorefresh
+      this.stopAutorefreshModel();
+    },
+
+  });
 
 });
